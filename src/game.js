6562796e5a6ed0e6cd9d99540add3180a3d16ee0
AlejandroRDev <iron-man@avengers.com>
2022-02-13 11:20:01 +0100

    implementing levels

--- src/game.js
+++ src/game.js
@@ -2,16 +2,15 @@ import Paddle from "./paddle.js";
 import InputHandler from "./input.js";
 import Stone from "./stone.js";
 import Harry from "./harry.js";
-import { buildLevel, levelOne } from "./levels.js";
+import { buildLevel, levelOne, levelTwo, levelThree, levelFour, levelFive } from "./levels.js";
 
 const gameState = {
   Paused: 0,
   Running: 1,
   Menu: 2,
   GameOver: 3,
+  NewLevel: 4,
 };
-
-
 
 export default class Game {
   constructor(gameWidth, gameHeight, harrysPerRow) {
@@ -20,27 +19,37 @@ export default class Game {
     this.gamestate = gameState.Menu;
     this.paddle = new Paddle(this);
     this.stone = new Stone(this);
+    this.harrys = [];
     this.gameObjects = [];
-    new InputHandler(this.paddle, this);
     this.lives = 3;
+
+    this.levels = [levelOne, levelTwo, levelThree, levelFour, levelFive];
+    this.currentLevel = 0;
+
+    new InputHandler(this.paddle, this);
   }
 
   start() {
     //con esto conseguimos que solo desde el menu se pueda iniciar/reset el juego.
-    if(this.gamestate !== gameState.Menu && this.gamestate !== gameState.GameOver) return;
+    if (
+      this.gamestate !== gameState.Menu &&
+      this.gamestate !== gameState.GameOver &&
+      this.gamestate !== gameState.NewLevel
+    )
+      return;
 
-    let harry = buildLevel(this, levelOne);
+    this.harrys = buildLevel(this, this.levels[this.currentLevel]);
+    this.stone.reset();
+    this.gameObjects = [this.stone, this.paddle];
 
-    this.gameObjects = [this.stone, this.paddle, ...harry];
- 
     this.gamestate = gameState.Running;
-
   }
 
   update(deltaTime) {
-    if(this.lives === 0) {
+    if (this.lives === 0) {
       this.gamestate = gameState.GameOver;
-    this.lives = 3}
+      this.lives = 3;
+    }
 
     if (
       this.gamestate === gameState.Paused ||
@@ -48,17 +57,26 @@ export default class Game {
     )
       return;
 
-    this.gameObjects.forEach((object) => object.update(deltaTime));
+    if (this.harrys.length === 0) {
+      console.log("new level");
+      this.currentLevel++;
+      this.gamestate = gameState.NewLevel;
+      this.start();
+    }
 
-    this.gameObjects = this.gameObjects.filter(
-      (object) => !object.markedForDelete
+    [...this.gameObjects, ...this.harrys].forEach((object) =>
+      object.update(deltaTime)
     );
+
+    this.harrys = this.harrys.filter((harry) => !harry.markedForDelete);
   }
 
   draw(context) {
-    this.gameObjects.forEach((object) => object.draw(context));
+    [...this.gameObjects, ...this.harrys].forEach((object) =>
+      object.draw(context)
+    );
     this.drawLives(context);
-//STATE PAUSED
+    //STATE PAUSED
     if (this.gamestate === gameState.Paused) {
       context.rect(0, 0, this.gameWidth, this.gameHeight);
       context.fillStyle = "rgba(0,0,0,0.5)"; //Usamos RGBA para poder meterle opacity.
@@ -68,7 +86,7 @@ export default class Game {
       context.textAlign = "center";
       context.fillText("PAUSED", this.gameWidth / 2, this.gameHeight / 2);
     }
-//STATE MENU
+    //STATE MENU
     if (this.gamestate === gameState.Menu) {
       context.rect(0, 0, this.gameWidth, this.gameHeight);
       context.fillStyle = "black"; //Usamos RGBA para poder meterle opacity.
@@ -76,26 +94,33 @@ export default class Game {
       context.font = "40px Arial";
       context.fillStyle = "whitesmoke";
       context.textAlign = "center";
-      context.fillText("Press SPACEBAR", this.gameWidth / 2, this.gameHeight / 2);
+      context.fillText(
+        "Press SPACEBAR",
+        this.gameWidth / 2,
+        this.gameHeight / 2
+      );
     }
-//STATE GAMEOVER
-if (this.gamestate === gameState.GameOver) {
-  context.rect(0, 0, this.gameWidth, this.gameHeight);
-  context.fillStyle = "black"; //Usamos RGBA para poder meterle opacity.
-  context.fill();
-  context.font = "40px Arial";
-  context.fillStyle = "whitesmoke";
-  context.textAlign = "center";
-  context.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2);
-  context.fillText("Press SPACEBAR to defeat Harry", this.gameWidth / 2, this.gameHeight - 50 / 2);
-}
-
+    //STATE GAMEOVER
+    if (this.gamestate === gameState.GameOver) {
+      context.rect(0, 0, this.gameWidth, this.gameHeight);
+      context.fillStyle = "black"; //Usamos RGBA para poder meterle opacity.
+      context.fill();
+      context.font = "40px Arial";
+      context.fillStyle = "whitesmoke";
+      context.textAlign = "center";
+      context.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2);
+      context.fillText(
+        "Press SPACEBAR to defeat Harry",
+        this.gameWidth / 2,
+        this.gameHeight - 50 / 2
+      );
+    }
   }
 
-  drawLives(context, gameWidth ) {
+  drawLives(context, gameWidth) {
     context.font = "24px Arial";
     context.fillStyle = "#fccd32";
-    context.fillText("Lives: " + this.lives, this.gameWidth-65, 35);
+    context.fillText("Lives: " + this.lives, this.gameWidth - 65, 35);
   }
   pause() {
     if (this.gamestate == gameState.Paused) {
@@ -105,6 +130,5 @@ if (this.gamestate === gameState.GameOver) {
     }
   }
 }
-
 
 //font
